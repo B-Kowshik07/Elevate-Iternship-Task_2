82767ada9944322f887b78eca434266f1ca1c5f8
Boggarpu Venkata Kowshik <venkatakowshik07@gmail.com>
2025-08-05 11:17:57 +0530

    Add files via upload
    A simple To-Do List CLI program in Python lets you manage your daily tasks using text commands in the terminal. You can add new tasks, see your current list, and remove tasks you’ve finished. It is an easy way to keep track of what you need to do and is a great project for learning how to write basic Python code.

---
+++ To do list.py
@@ -0,0 +1,50 @@
+
+
+todo_list = []
+
+def show_menu():
+    print("\n======To-Do List Menu:========")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Remove Task")
+    print("4. Exit")
+
+while True:
+    show_menu()
+    choice = input("Enter your choice (1/2/3/4): ")
+
+    if choice == "1":
+        task = input("Enter the task: ")
+        todo_list.append(task)
+        print("Task added.")
+
+    elif choice == "2":
+        if not todo_list:
+            print("No tasks in the list.")
+        else:
+            print("\nYour Tasks:")
+            for idx, task in enumerate(todo_list, 1):
+                print(f"{idx}. {task}")
+
+    elif choice == "3":
+        if not todo_list:
+            print("No tasks to remove.")
+        else:
+            for idx, task in enumerate(todo_list, 1):
+                print(f"{idx}. {task}")
+            try:
+                remove_idx = int(input("Enter task number to remove: ")) - 1
+                if 0 <= remove_idx < len(todo_list):
+                    removed = todo_list.pop(remove_idx)
+                    print(f"Removed task : {removed}")
+                else:
+                    print("Invalid task number.")
+            except ValueError:
+                print("Please enter a valid number.")
+
+    elif choice == "4":
+        print("Exiting To-Do List. Goodbye!👋")
+        break
+
+    else:
+        print("Invalid choice. Please enter a number from 1 to 4.")
